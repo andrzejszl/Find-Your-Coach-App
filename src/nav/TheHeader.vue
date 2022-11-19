f71e8f932fa5c90651601059784e8516f5f989fe
andrzejszl <andrzej.sz123@gmail.com>
2022-11-19 20:56:52 +0100

    css fix

--- src/nav/TheHeader.vue
+++ src/nav/TheHeader.vue
@@ -6,34 +6,66 @@
   </header>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
 
 header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
   display: flex;
+  justify-content: center;
   align-items: center;
-  background-color: purple;
-  padding: 10px 30px;
-  h1 {
-    color: white;
-    margin-right: auto;
-    margin-left: 50px;
-    font-size: 1.6rem;
-  }
-  a {
-    font-size: 1.2rem;
-    text-decoration: none;
-    color: #333;
-    font-weight: bold;
-    border-bottom: 2px solid transparent;
-    padding-bottom: 0.25rem;
-    margin: 0 20px;
-  }
-  a:hover,
-  a:active,
-  a.router-link-active {
-    color: #45006d;
-    border-color: #45006d;
-  }
+  padding: 0 60px;
 }
 
+header a {
+  text-decoration: none;
+  color: #f391e3;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #f391e3;
+}
+
+h1 {
+  margin: 0;
+  margin-right: auto;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
 </style>
