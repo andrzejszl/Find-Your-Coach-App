8c894e4a879b9bf720e3227811262f1c40efc1d1
andrzejszl <andrzej.sz123@gmail.com>
2022-11-20 21:45:44 +0100

    add local caching for coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -23,7 +23,8 @@ export default {
             id: userId,
         })
     },
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if (!payload.forceRefresh && !context.getters.shouldUpdate) return;
         const response= await fetch(`https://find-a-coach-13501-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`);
         const responseData = await response.json()
 
@@ -44,6 +45,7 @@ export default {
             };
             coaches.push(coach)
         }
-        context.commit('setCoaches', coaches)
+        context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
