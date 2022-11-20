9dd449c9b808ba3b9b086152fe28e4f93e37b596
andrzejszl <andrzej.sz123@gmail.com>
2022-11-21 00:05:55 +0100

    add not found page for wrong path

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,13 +1,8 @@
 <template>
-    <div></div>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be found - maybe check all our <router-link to="/coaches">coaches</router-link>.</p>
+        </base-card>
+    </section>
 </template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
