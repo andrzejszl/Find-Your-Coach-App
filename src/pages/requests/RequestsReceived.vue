d03e21c624d30038241e9543561a19f48946864f
andrzejszl <andrzej.sz123@gmail.com>
2022-11-20 16:23:17 +0100

    add requests feature

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,13 +1,51 @@
 <template>
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repudiandae voluptatum dolorem nobis doloribus illo consequuntur ex, temporibus sed magnam, commodi tempora delectus similique sint esse explicabo corrupti, nam unde!</div>
+    <section>
+        <base-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+            <ul v-if="hasRequests">
+                <request-item 
+                    v-for="req in receivedRequests" 
+                    :key="req.id" 
+                    :email="req.userEmail"
+                    :message="req.message"
+                ></request-item>
+            </ul>
+            <h3 v-else>You haven't reveived any requests!</h3>
+        </base-card>
+    </section>
 </template>
 
 <script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
     export default {
-        
+        components: { RequestItem },
+        computed: {
+            receivedRequests() {
+                return this.$store.getters['requests/requests']
+            },
+            hasRequests() {
+                return this.$store.getters['requests/hasRequests']
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js'
 
 export default createStore({
   modules: { 
     coaches: coachesModule,
+    requests: requestsModule,
   },
   state() {
     return {
