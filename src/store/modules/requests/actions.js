82458149ad053ceba36d4913a24302d52272b5fe
andrzejszl <andrzej.sz123@gmail.com>
2022-11-21 18:54:50 +0100

    attach token to requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -21,7 +21,8 @@ export default {
         },
         async fetchRequests(context) {
             const coachId = context.rootGetters.userId;
-            const response = await fetch(`https://find-a-coach-13501-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`);
+            const token = context.rootGetters.token
+            const response = await fetch(`https://find-a-coach-13501-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=${token}`);
             const responseData = await response.json();
 
             if (!response.ok) {
